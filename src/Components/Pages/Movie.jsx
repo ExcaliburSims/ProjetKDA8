c93ebd275cb70674088c47394ad0fdb5a5d89e83
simeonzilu <ximeonzilu@gmail.com>
2021-05-18 09:16:43 +0100

    feat:add content on movie component

--- src/Components/Pages/Movie.jsx
+++ src/Components/Pages/Movie.jsx
@@ -1,11 +1,98 @@
-import React from 'react';
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import Card from '../Card/Card';
+import styled from "styled-components";
+import { FaSearch /* , FaTimes */ } from "react-icons/fa";
+
+
+export const SearchBloc=styled.div`
+width:25vw;
+position:relative;
+left:55%;
+`
+export const SearchBar = styled.form`
+	font-size: 1.1rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	background-color:  #282c34;
+	padding: 0.5rem 0.8rem;
+	border-radius: 100px;
+	color: rgba(202, 206, 221, 0.8);
+	box-shadow: 0.2rem 0.2rem 0.5rem #ccc;
+	.Input {
+		margin: 0 0.5rem;
+		width: 30%;
+		border: none;
+		outline: none;
+		background-color:  #282c34;
+		color: rgba(202, 206, 221, 0.8);
+		::placeholder {
+			color: rgba(202, 206, 221, 0.8);
+		}
+	}
+`;
 
 const Movie = () => {
-    return (
-        <div>
-            <h1>Movie</h1>
-        </div>
-    );
-};
+	const [movies, setMovies] = useState([]);
+	const linkImage = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
+	const [searchMovie, setSearchMovie] = useState("");
 
+	useEffect(() => {
+		axios
+			.get(
+				'https://api.themoviedb.org/3/movie/popular?api_key=c830918bda0a4dcad664e68958cc9d71&page=1'
+			)
+			.then((res) => setMovies(res.data.results))
+			.catch((e) => console.error(e));
+	}, []);
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		fetch(
+			"https://api.themoviedb.org/3/search/movie?api_key=c830918bda0a4dcad664e68958cc9d71&query=" +
+				searchMovie
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				setMovies(data.results);
+			});
+	};
+
+	const handleOnChange = (e) => {
+		setSearchMovie(e.target.value);
+	};
+
+	return (
+		<>
+			<SearchBloc>
+				<SearchBar onSubmit={handleOnSubmit}>
+					<input
+						type="text"
+						class="Input"
+						placeholder="Search...."
+						onChange={handleOnChange}
+						value={searchMovie}
+					/>
+				</SearchBar>
+			</SearchBloc>
+
+			<div className="movie-container">
+				{movies.map((movie) => {
+					return (
+						<div>
+							<Card
+								link={linkImage + movie.poster_path}
+								title={movie.title}
+								vote_average={movie.vote_average}
+								id={movie.id}
+								overview={movie.overview}
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
 export default Movie;
